645f1d399da32175dca6fe535ac515596dd91133
Pablo Tabares Hernandez <pablother3@gmail.com>
2019-09-03 10:13:14 -0500

    first behavior with hooks

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [outputText, setoutputText] = useState('Open up App.js to start working on your app!')
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button
+        title="Change text"
+        onPress={() => setoutputText('The text changed')}
+      ></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
